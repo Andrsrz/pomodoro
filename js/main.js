e33f64ce407fa7406216fe19c2e8c08f0c1d6a29
Andres Ruiz <andrsruiz@gmail.com>
2020-05-13 11:35:31 -0500

    Create Grid

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-console.log("works");
+const COLUMNS = 8;
+const ROWS = 3;
+const DEFAULT_SESSION = 25; /* Minutes */
+const DEFAULT_BREAK = 5; /* Minutes */
+
+function createGrid(){
+	let divGrid = getGridContainer();
+	divGrid.setAttribute("style", "grid-template-columns: repeat(" + COLUMNS + ", 1fr); \
+									grid-template-rows: repeat(" + ROWS + ", 1fr);");
+}
+
+function getGridContainer(){ return document.getElementById("grid"); }
+
+function populateGrid(sessionMinutes, breakMinutes){}
+
+createGrid();
