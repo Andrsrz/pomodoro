eff4b7c353e967cf299de8396f67fbb996329f99
Andres Ruiz <andrsruiz@gmail.com>
2020-05-13 12:02:14 -0500

    Create Buttons

--- js/main.js
+++ js/main.js
@@ -7,10 +7,45 @@ function createGrid(){
 	let divGrid = getGridContainer();
 	divGrid.setAttribute("style", "grid-template-columns: repeat(" + COLUMNS + ", 1fr); \
 									grid-template-rows: repeat(" + ROWS + ", 1fr);");
+	populateGrid(DEFAULT_SESSION, DEFAULT_BREAK, divGrid);
 }
 
 function getGridContainer(){ return document.getElementById("grid"); }
 
-function populateGrid(sessionMinutes, breakMinutes){}
+function populateGrid(sessionMinutes, breakMinutes, whatGrid){
+	buttons = createButtons();
+	for(let i = 0; i < buttons.length; i++){
+		whatGrid.appendChild(buttons[i]);
+	}
+}
+
+function createButtons(){
+	let buttons = []
+	let btnPause = document.createElement("button");
+	btnPause.setAttribute("id", "buttonPause");
+	btnPause.innerHTML = "PAUSE";
+	buttons.push(btnPause);
+	let btnRun = document.createElement("button");
+	btnRun.setAttribute("id", "buttonRun");
+	btnRun.innerHTML = "START";
+	buttons.push(btnRun);
+	let btnStop = document.createElement("button");
+	btnStop.setAttribute("id", "buttonStop");
+	btnStop.innerHTML = "STOP";
+	buttons.push(btnStop);
+
+	let columnStart = 2;
+	let columnEnd = columnStart + 2;
+	for(let i = 0; i < buttons.length; i++){
+		buttons[i].setAttribute("style", "grid-column-start:" + columnStart +" ;\
+										grid-column-end:" + columnEnd + ";\
+										grid-row-start: 1;\
+										grid-row-end: 2;");
+		columnStart += 2;
+		columnEnd = columnStart + 2;
+	}
+
+	return buttons;
+}
 
 createGrid();
